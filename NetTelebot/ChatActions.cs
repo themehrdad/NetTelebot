e8e46c71ea01b16ea729b63686721ff687c5f1cd
Alex <vertigra.developer@gmail.com>
2017-06-15 01:56:02 +1000

    Remove unused using directive

--- NetTelebot/ChatActions.cs
+++ NetTelebot/ChatActions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NetTelebot
+﻿namespace NetTelebot
 {
     /// <summary>
     /// Use this enum when you need to tell the user that something is happening on the bot's side. The status is set for 5 seconds or less (when a message arrives from your bot, Telegram clients clear its typing status).
--- NetTelebot/IFile.cs
+++ NetTelebot/IFile.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NetTelebot
+﻿namespace NetTelebot
 {
     /// <summary>
     /// This interface is used when calling SendPhoto, SendAudio, SendVideo and SendDocument. It can be a NewFile or Existing class.
