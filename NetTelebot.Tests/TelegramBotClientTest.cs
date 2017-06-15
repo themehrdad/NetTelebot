bdab9a119284516cc2169937734ea0576ae2fec2
Alex <vertigra.developer@gmail.com>
2017-06-16 01:30:16 +1000

    Add nunit test. Test send message.

---
+++ NetTelebot.Tests/TelegramBotClientTest.cs
@@ -0,0 +1,43 @@
+﻿using NetTelebot.Tests.Utils;
+using NUnit.Framework;
+
+namespace NetTelebot.Tests
+{
+    [TestFixture]
+    internal class TelegramBotClientTest : IWindowsCredential
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public void TestSendMessage()
+        {
+            bool sendMessage = TelegramBot().SendMessage(GetTelegramCredential("NetTelebotTest").ChatId, "Test").Ok;
+            Assert.True(sendMessage);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private TelegramBotClient TelegramBot()
+        {
+            TelegramBotClient telegramBotClient = new TelegramBotClient()
+            {
+                Token = GetTelegramCredential("NetTelebotTest").Token
+            };
+
+            return telegramBotClient;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="botAlias"></param>
+        /// <returns></returns>
+        public TelegramCredentials GetTelegramCredential(string botAlias)
+        {
+            return new WindowsCredential().GetTelegramCredential(botAlias);
+        }
+    }
+}
