a638b689432ea1f6c5be4e745944d17ab5bf9bab
Alex <vertigra.developer@gmail.com>
2017-06-20 02:16:38 +1000

    Update MessageInfo.  Added properties with an existing or simple return type.

--- NetTelebot/MessageInfo.cs
+++ NetTelebot/MessageInfo.cs
@@ -5,7 +5,8 @@ using System;
 namespace NetTelebot
 {
     /// <summary>
-    /// This object represents a message.
+    /// This object represents a message. 
+    /// API <link href="https://core.telegram.org/bots/api#message"></link>
     /// </summary>
     public class MessageInfo
     {
@@ -33,14 +34,19 @@ namespace NetTelebot
             Chat = ParseChat(jsonObject["chat"].Value<JObject>());
             if (jsonObject["forward_from"] != null)
                 ForwardFrom = new UserInfo(jsonObject["forward_from"].Value<JObject>());
-            if(jsonObject["forward_date"]!=null)
+            if (jsonObject["forward_from_message_id"] != null)
+                ForwardFromMessageId = jsonObject["forward_from_message_id"].Value<int>();
+            if (jsonObject["forward_date"]!=null)
             {
                 ForwardDateUnix = jsonObject["forward_date"].Value<int>();
                 ForwardDate = ForwardDateUnix.ToDateTime();
             }
             if(jsonObject["reply_to_message"]!=null)
-            {
                 ReplyToMessage = new MessageInfo(jsonObject["reply_to_message"].Value<JObject>());
+            if (jsonObject["edit_date"] != null)
+            {
+                EditDateUnix = jsonObject["edit_date"].Value<int>();
+                EditDate = EditDateUnix.ToDateTime();
             }
             if (jsonObject["text"] != null)
                 Text = jsonObject["text"].Value<string>();
@@ -54,6 +60,8 @@ namespace NetTelebot
                 Sticker = new StickerInfo(jsonObject["sticker"].Value<JObject>());
             if (jsonObject["video"] != null)
                 Video = new VideoInfo(jsonObject["video"].Value<JObject>());
+            if (jsonObject["caption"] != null)
+                Caption = jsonObject["caption"].Value<string>();
             if (jsonObject["contact"] != null)
                 Contact = new ContactInfo(jsonObject["contact"].Value<JObject>());
             if (jsonObject["location"] != null)
@@ -70,15 +78,26 @@ namespace NetTelebot
                 DeleteChatPhoto = true;
             if (jsonObject["group_chat_created"] != null)
                 GroupChatCreated = true;
+            if (jsonObject["supergroup_chat_created"] != null)
+                SuperGroupChatCreated = true;
+            if (jsonObject["channel_chat_created"] != null)
+                ChannelChatCreated = true;
+            if (jsonObject["migrate_to_chat_id"] != null)
+                MigrateToChatId = jsonObject["migrate_to_chat_id"].Value<int>();
+            if (jsonObject["migrate_from_chat_id"] != null)
+                MigrateFromChatId = jsonObject["migrate_from_chat_id"].Value<int>();
+            if (jsonObject["pinned_message"] != null)
+                PinnedMessage = new MessageInfo(jsonObject["pinned_message"].Value<JObject>());
         }
 
         private static IConversationSource ParseChat(JObject jsonObject)
         {
             if (jsonObject["title"] != null)
                 return new GroupChatInfo(jsonObject);
-            else
-                return new UserInfo(jsonObject);
+
+            return new UserInfo(jsonObject);
         }
+
         /// <summary>
         /// Unique message identifier
         /// </summary>
@@ -103,6 +122,13 @@ namespace NetTelebot
         /// Optional. For forwarded messages, sender of the original message
         /// </summary>
         public UserInfo ForwardFrom { get; private set; }
+
+        //todo add (IConversationSource) ForwardFromChat
+
+        /// <summary>
+        /// Optional. For forwarded channel posts, identifier of the original message in the channel
+        /// </summary>
+        public int ForwardFromMessageId { get; private set; }
         /// <summary>
         /// Optional. For forwarded messages, date the original message was sent in Unix time
         /// </summary>
@@ -116,9 +142,20 @@ namespace NetTelebot
         /// </summary>
         public MessageInfo ReplyToMessage { get; private set; }
         /// <summary>
+        /// Optional. Date the message was last edited in Unix time
+        /// </summary>
+        public int EditDateUnix { get; private set; }
+        /// <summary>
+        /// Optional. Date the message was last edited
+        /// </summary>
+        public DateTime EditDate { get; private set; }
+        /// <summary>
         /// Optional. For text messages, the actual UTF-8 text of the message
         /// </summary>
         public string Text { get; private set; }
+
+        //todo add (MessageEntity) Entities
+
         /// <summary>
         /// Optional. Message is an audio file, information about the file
         /// </summary>
@@ -127,6 +164,9 @@ namespace NetTelebot
         /// Optional. Message is a general file, information about the file
         /// </summary>
         public DocumentInfo Document { get; private set; }
+
+        //todo add (Game) Game
+
         /// <summary>
         /// Optional. Message is a photo, available sizes of the photo
         /// </summary>
@@ -139,6 +179,16 @@ namespace NetTelebot
         /// Optional. Message is a video, information about the video
         /// </summary>
         public VideoInfo Video { get; private set; }
+
+        //todo add (Voice) Voice
+        //todo add (VideoNote) VideoNote
+        //todo add (Array of User) NewChatMembers
+
+
+        /// <summary>
+        /// Optional. Caption for the document, photo or video, 0-200 characters
+        /// </summary>
+        public string Caption { get; private set; }
         /// <summary>
         /// Optional. Message is a shared contact, information about the contact
         /// </summary>
@@ -147,6 +197,9 @@ namespace NetTelebot
         /// Optional. Message is a shared location, information about the location
         /// </summary>
         public LocationInfo Location { get; private set; }
+
+        //todo add (Venue) Venue
+
         /// <summary>
         /// Optional. A new member was added to the group, information about them (this member may be bot itself)
         /// </summary>
@@ -171,5 +224,35 @@ namespace NetTelebot
         /// Optional. Informs that the group has been created
         /// </summary>
         public bool GroupChatCreated { get; private set; }
+        /// <summary>
+        /// Optional. Service message: the supergroup has been created. This field can‘t be received in a message coming through updates,
+        /// because bot can’t be a member of a supergroup when it is created. It can only be found in reply_to_message if someone replies to a very
+        /// first message in a directly created supergroup.
+        /// </summary>
+        public bool SuperGroupChatCreated { get; private set;  }
+        /// <summary>
+        /// Optional. Service message: the channel has been created. This field can‘t be received in a message coming through updates, 
+        /// because bot can’t be a member of a channel when it is created. It can only be found in reply_to_message if someone replies to a very
+        /// first message in a channel.
+        /// </summary>
+        public bool ChannelChatCreated { get; private set; }
+        /// <summary>
+        /// Optional. The group has been migrated to a supergroup with the specified identifier. This number may be greater than 32 bits and some
+        /// programming languages may have difficulty/silent defects in interpreting it. But it is smaller than 52 bits, so a signed 64 bit integer or double-precision
+        /// float type are safe for storing this identifier.
+        /// </summary>
+        public int MigrateToChatId { get; private set;  }
+        /// <summary>
+        /// Optional. The supergroup has been migrated from a group with the specified identifier. This number may be greater than 32 bits and some
+        /// programming languages may have difficulty/silent defects in interpreting it. But it is smaller than 52 bits, so a signed 64 bit integer or double-precision
+        /// float type are safe for storing this identifier.
+        /// </summary>
+        public int MigrateFromChatId { get; private set;  }
+        /// <summary>
+        /// Optional. Specified message was pinned. Note that the Message object in this field will not contain further reply_to_message fields even if it is itself a reply.
+        /// </summary>
+        public MessageInfo PinnedMessage { get; private set; }
+        //todo (Invoice) Invoice
+        //todo (SuccessfulPayment) SuccessfulPayment
     }
 }
