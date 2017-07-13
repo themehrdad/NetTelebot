49bc76f74a6b4e5e68c2e46f5808abd6e803733a
Alex <vertigra.developer@gmail.com>
2017-07-13 23:49:46 +1000

    Interim commit

---
+++ NetTelebot.Tests/TypeTestObject/UserInfoObject.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace NetTelebot.Tests.TypeTestObject
+{
+    internal class UserInfoObject
+    {
+        protected UserInfoObject()
+        {
+        }
+
+        /// <summary>
+        /// This object represents a Telegram user or bot. See <see href="https://core.telegram.org/bots/api#user">API</see>
+        /// </summary>
+        /// <returns><see cref="UserInfo"/></returns>
+        internal static JObject GetObject(int id, string firstName, string lastName, string username, string languageCode)
+        {
+
+            dynamic userInfo = new JObject();
+
+            userInfo.id = id;
+            userInfo.first_name = firstName;
+
+            return userInfo;
+        }
+}
