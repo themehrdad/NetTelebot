c510a9f350480e36215816622ba99e7870c9b0f1
Alex <vertigra.developer@gmail.com>
2017-08-04 23:12:47 +1000

    Close issue vertigra/NetTelebot-2.0#74 vertigra/NetTelebot-2.0#60 vertigra/NetTelebot-2.0#61 vertigra/NetTelebot-2.0#62 vertigra/NetTelebot-2.0#38

--- NetTelebot.Tests/MockServerObject/ResponseString.cs
+++ NetTelebot.Tests/MockServerObject/ResponseString.cs
@@ -2,17 +2,17 @@
 {
     internal static class ResponseString
     {
-        public static string mExpectedBodyForSendMessageResult =
+        internal static readonly string mExpectedBodyForSendMessageResult =
             @"{ ok: ""true"", result: { message_id: 123, date: 0, chat: { id: 123, type: ""private"" }}}";
 
-        public static string mExpectedBodyForGetUserProfilePhotos =
+        internal static readonly string mExpectedBodyForGetUserProfilePhotos =
             @"{ ok: ""true"", result: { total_count: 1, photos: [[ { file_id: ""123"", width: 123, height: 123 }, { file_id: ""456"", width: 456, height: 456 } ]] }}";
 
-        public static string mExpectedBodyForGetMe =
+        internal static readonly string mExpectedBodyForGetMe =
             @"{ ok: ""true"", result: { id: ""123"", first_name: ""FirstName"", username: ""username"" }}";
 
-        public static string mExpectedBodyForBooleanResult = @"{ ok: ""true"", result: ""true"" }";
+        internal static readonly string mExpectedBodyForBooleanResult = @"{ ok: ""true"", result: ""true"" }";
 
-        public static string mExpectedBodyForBadResponse = @"{ ok: ""false"", error_code: 401, description: ""Unauthorized"")";
+        internal static readonly string mExpectedBodyForBadResponse = @"{ ok: ""false"", error_code: 401, description: ""Unauthorized"")";
     }
 }
