fcfe00a7f1c1dbafc05a01ab462256360c88805c
Alex <vertigra.developer@gmail.com>
2017-08-05 00:08:46 +1000

    Test correction codecov result

--- NetTelebot.Tests/MockServerObject/MockServer.cs
+++ NetTelebot.Tests/MockServerObject/MockServer.cs
@@ -1,7 +1,9 @@
 ﻿using Mock4Net.Core;
+using NUnit.Framework;
 
 namespace NetTelebot.Tests.MockServerObject
 {
+    [TestFixture]
     internal static class MockServer
     {
         internal static FluentMockServer ServerOkResponse { get; private set; }
