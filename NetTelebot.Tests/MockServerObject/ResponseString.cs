431c696332c0ccd112c61b7065be03522a3d8945
Alex <vertigra.developer@gmail.com>
2017-08-04 23:50:41 +1000

    Fix small codacy issue

--- NetTelebot.Tests/MockServerObject/ResponseString.cs
+++ NetTelebot.Tests/MockServerObject/ResponseString.cs
@@ -2,17 +2,17 @@
 {
     internal static class ResponseString
     {
-        internal static readonly string mExpectedBodyForSendMessageResult =
+        internal const string mExpectedBodyForSendMessageResult =
             @"{ ok: ""true"", result: { message_id: 123, date: 0, chat: { id: 123, type: ""private"" }}}";
 
-        internal static readonly string mExpectedBodyForGetUserProfilePhotos =
+        internal const string mExpectedBodyForGetUserProfilePhotos =
             @"{ ok: ""true"", result: { total_count: 1, photos: [[ { file_id: ""123"", width: 123, height: 123 }, { file_id: ""456"", width: 456, height: 456 } ]] }}";
 
-        internal static readonly string mExpectedBodyForGetMe =
+        internal const string mExpectedBodyForGetMe =
             @"{ ok: ""true"", result: { id: ""123"", first_name: ""FirstName"", username: ""username"" }}";
 
-        internal static readonly string mExpectedBodyForBooleanResult = @"{ ok: ""true"", result: ""true"" }";
+        internal const string mExpectedBodyForBooleanResult = @"{ ok: ""true"", result: ""true"" }";
 
-        internal static readonly string mExpectedBodyForBadResponse = @"{ ok: ""false"", error_code: 401, description: ""Unauthorized"")";
+        internal const string mExpectedBodyForBadResponse = @"{ ok: ""false"", error_code: 401, description: ""Unauthorized"")";
     }
 }
