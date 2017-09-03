35411f99af8369041e48ddc5bfc9fbd18e7afbd0
Alex <vertigra.developer@gmail.com>
2017-09-04 00:15:55 +1000

    Refact CurrencyTest. Move create request and response method to separate class. Added CountriesTest... in process

---
+++ NetTelebot.Tests/TestForDiscrepanciesWithApi/CountriesTest.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using RestSharp;
+
+namespace NetTelebot.Tests.TestForDiscrepanciesWithApi
+{
+    [TestFixture]
+    internal class CountriesTest
+    {
+        private static readonly RequestToUri mRequestToUri =
+            new RequestToUri(new RestClient("https://raw.githubusercontent.com"));
+
+        //private readonly Dictionary<string, string> mKeyList =
+        //    mRequestToUri.GetObject("/mledoze/countries/blob/master/countries.json");
+
+        [Test, Ignore("In process")]
+        public void TestRequest()
+        {
+            Dictionary<string, string> mKeyList =
+                mRequestToUri.GetObject("/mledoze/countries/master/countries.json");
+
+            foreach (var keys in mKeyList.Keys)
+            {
+                Console.WriteLine(keys);
+            }
+        }
+
+    }
+}
---
+++ NetTelebot.Tests/TestForDiscrepanciesWithApi/RequestToUri.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mime;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Deserializers;
+
+namespace NetTelebot.Tests.TestForDiscrepanciesWithApi
+{
+    internal class RequestToUri
+    {
+        private readonly RestClient mRestClient;
+
+        internal RequestToUri(RestClient restClient)
+        {
+            mRestClient = restClient;
+        }
+
+        internal Dictionary<string, string> GetObject(string uri)
+        {
+            RestRequest request = NewRequest(uri);
+            RestResponse response = (RestResponse)NewResponse(request);
+            
+            return DeserealizeJson(response);
+        }
+
+        private static RestRequest NewRequest(string uri)
+        {
+            return new RestRequest(uri, Method.GET);
+        }
+
+        private IRestResponse NewResponse(IRestRequest request)
+        {
+            IRestResponse response = mRestClient.Execute(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+                return response;
+
+            throw new Exception(response.StatusDescription);
+        }
+
+        private static Dictionary<string, string> DeserealizeJson(IRestResponse response)
+        {
+            JsonDeserializer deserial = new JsonDeserializer();
+
+           // deserial.Deserialize<List<Dictionary<string, string>>>(response);
+
+            return deserial.Deserialize<Dictionary<string, string>>(response);
+        }
+    }
+}
