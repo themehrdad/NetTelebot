fb8e0d5f793f09ecf1322b010bf39c1eb761aba2
Nesterov Alexey <vertigra.developer@gmail.com>
2017-10-19 14:54:01 +1000

    Fix property name TotalAmmount -> TotalAmount in PreCheckoutQueryInfo. Added: LabelPrice (as LabelPriceInfo) type and ShippingOption (as ShippingOptionInfo)

---
+++ NetTelebot/Type/Payment/ShippingOptionInfo.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace NetTelebot.Type.Payment
+{
+    /// <summary>
+    /// This object represents one shipping option.
+    /// </summary>
+    public class ShippingOptionInfo
+    {
+        //todo add test
+        internal ShippingOptionInfo()
+        {   
+        }
+
+        internal ShippingOptionInfo(JObject jsonObject)
+        {
+            Id = jsonObject["id"].Value<string>();
+            Title = jsonObject["title"].Value<string>();
+            LabelPrice = LabelPriceInfo.ParseArray(jsonObject["prices"].Value<JArray>());
+        }
+        
+        /// <summary>
+        /// Shipping option identifier
+        /// </summary>
+        public string Id { get; private set; }
+
+        /// <summary>
+        /// Option title
+        /// </summary>
+        public string Title { get; private set; }
+
+        /// <summary>
+        /// List of price portions
+        /// </summary>
+        public LabelPriceInfo[] LabelPrice { get; internal set; }
+    }
+}
